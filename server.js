4821e3eb39ebadad2aa823265d22dfe6a72f3059
Luke Hampton <lukehampton6@gmail.com>
2021-07-06 18:24:58 -0400

    connected to database

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const mysql = require('mysql2');
 const express = require("express");
 
 const PORT = process.env.PORT || 3001;
@@ -5,6 +6,20 @@ const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: 'Ponceinlet8!',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
 
 app.use((req, res) => {
   res.status(404).end();
